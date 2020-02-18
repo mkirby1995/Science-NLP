51f4499c2e87eac449c9fe64faa01909fc4a0b91
Matt.Kirby <mkirby3@angelo.edu>
2020-02-18 12:48:37 -0600

    update

--- process_and_store.py
+++ process_and_store.py
@@ -13,28 +13,23 @@ from gensim.models import CoherenceModel
 
 from Processing import Processor
 
-nlp = spacy.load('en_core_web_lg')
-
 subprocess.call(['./pdf_converter'])
 
 path = 'txt_files/'
 
-files = []
 # r=root, d=directories, f = files
 for r, d, f in os.walk(path):
-    for file in f:
-        if '.txt' in file:
-            files.append(os.path.join(r, file))
-            
-            
-processor = Processor(nlp)
+    files = [os.path.join(r, file) for file in f if '.txt' in file]
+
+processor = Processor(spacy.load('en_core_web_lg'))
+
 
 storage = {}
 
 for f in files:
     file = f[len(path):-4]
     storage[file] = processor.process(f, export=False)
-    
-    
+
+
 with open('documents' + '.json', 'w') as outfile:
     json.dump(storage, outfile)
